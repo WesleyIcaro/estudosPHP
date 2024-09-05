7426df98e02b813a93db87323d95028b20104a25
Wesley Icaro Lima dos Santos <wesleyicaro4@gmail.com>
2024-09-05 15:01:12 -0300

    New class.

---
+++ exFuncoesAritmeticas/index.php
@@ -0,0 +1,32 @@
+<?php 
+// $valor = abs(-5476); // Deixa o valor positivo
+// print "A respota é $valor";
+
+// $numero = base_convert(254, 10, 16); // converte a base do número, tenho que buscar entender melhor.
+// print "A respota é $numero";
+
+// $valor = intdiv(5, 2); // Divisão de valor inteiro
+// print "A respota é $valor"; // valor = 2
+
+// $valor = max(53432, 232432, 324324, 2342, 234, 23, 1234, 2355,234 , 3 ,23, 23432); // Maior valor entre os números
+// print "A respota é $valor"; // valor = 324324
+
+// $valor = min(53432, 232432, 324324, 2342, 234, 23, 1234, 2355,234 , 3 ,23, 23432); // Menor valor entre os números
+// print "A respota é $valor"; // valor = 3
+
+// $valor = M_PI; // Valor de PI
+// $valor = pi(); // Valor de PI
+// print "A respota é $valor"; // valor = pi
+
+// $valor = pow(3, 3); // 3 elevado a 3, porém a função pow perde a ordem de precedência
+// print "A respota é $valor"; // valor = 27
+
+// $valor = sqrt(49); // Raiz quadrada de 49
+// print "A respota é $valor"; // valor = 7
+
+// $valor = 81 ** (1/2); // Raiz quadrada de 81
+// print "A respota é $valor"; // valor = 9
+
+$valor = 27 ** (1/3); // Raiz cúbica de 27
+print "A respota é $valor"; // valor = 3
+?>
