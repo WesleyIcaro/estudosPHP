1f7de25cc91485d4396f32bf4910ed0eb0a326ad
21161408-5 <wesleyicaro4@gmail.com>
2024-09-05 23:13:31 -0300

    New class about math and challengers in php.

---
+++ des001/ger.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sorteio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <main>
+        <h1>Trabalhando com número aleatórios</h1>
+        <p>Gerando um número aleatório entre 0 e 100</p>
+
+        <?php
+
+        if(isset($_POST['random'])) {
+            $sorteio = rand(0, 100);
+            echo "<p>O valor gerado foi $sorteio</p>";
+        }        
+
+        ?>
+
+        <form method="post">            
+            <button type="submit" name="random">Gerar outro</button>
+        </form>
+    </main>
+</body>
+
+</html>
---
+++ exOperadoresAritmeticos/index.php
@@ -0,0 +1,5 @@
+<?php 
+$valor = "4 " + "4"; // Em PHP o operador tenta converter os valores que são somados, se quiser concatenar é necessário usar o .
+// E se não for número dar erro, sendo que o . é operador de string
+echo "O valor é $valor"
+?>
